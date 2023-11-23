1fb4ec751171b48cd7b179e9b75a472ba39a4c0d
Kamil Królikowski <kkrolikowski@xtm-intl.com>
2023-11-24 00:18:50 +0100

    feat: get notes list in indexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
+use App\Service\GoogleKeepApiService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(UserService $userService, GoogleKeepApiService $googleKeepApiService): Response
     {
+        $notesList = [];
+        $user = $userService->getCurrentUser();
+        if ($user) {
+            $notesList = $googleKeepApiService->getListOfNotes($user->getToken());
+        }
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'notes' => $notesList
         ]);
     }
 }
